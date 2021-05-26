29f15afd992e68639209bfbb08388fa9f4724b1c
Kolawole Ojo <philliphopeojo@gmail.com>
2021-05-26 19:21:42 +0100

    Layout setup

---
+++ src/layout/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navigation />
+    <h1>Default template</h1>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from "../components/layouts-partials/Nav";
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from "vue";
+import axios from "axios";
+
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./_router";
+import store from "./_store";
+
+import Default from "./layout/Default.vue";
+import Alternative from "./layout/Alternative.vue";
+import helpers from "./_helpers/index";
+let { DOMAIN, USER } = helpers;
+
+axios.defaults.baseURL = DOMAIN;
+axios.defaults.headers.common["Authorization"] = USER;
+// eslint-disable-next-line
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
+Vue.component("auth-layout", Alternative);
+Vue.component("default-layout", Default);
+
+Vue.use(axios);
+Vue.use(store);
+Vue.use(router);
 
 Vue.config.productionTip = false;
 
