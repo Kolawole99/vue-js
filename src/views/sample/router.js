995068f061923cee7c24ebb067efcf6b5eb43c2a
Kolawole Ojo <philliphopeojo@gmail.com>
2021-05-27 00:18:37 +0100

    Router webpack for pages update

--- src/views/sample/router.js
+++ src/views/sample/router.js
@@ -3,20 +3,32 @@ export default [
   {
     path: "/sample",
     meta: { layout: "Alternative" },
-    component: () => import(/* webpackChunkName: "about" */ "./Sample.vue"),
+    component: () => import(/* webpackChunkName: "sample view" */ "./Sample.vue"),
     children: [
       {
         path: "",
         name: "AllSamples",
-        meta: {
-          layout: "Alternative",
-          title: "Sample Page",
-        },
-        component: () => import("../../components/views-partials/sample/AllSamples.vue"),
+        meta: { layout: "Alternative", title: "Samples Page" },
+        component: () => import(/* webpackChunkName: "samples_view" */ "../../components/views-partials/sample/AllSamples.vue"),
       },
-      { path: ":id", name: "One Sample", meta: { layout: "Alternative" }, component: () => import("../../components/views-partials/sample/OneSample.vue") },
-      { path: "create", name: "Create Sample", meta: { layout: "Alternative" }, component: () => import("../../components/views-partials/sample/CreateSample.vue") },
-      { path: ":id/edit", name: "Edit Sample", meta: { layout: "Alternative" }, component: () => import("../../components/views-partials/sample/EditSample.vue") },
+      {
+        path: ":id",
+        name: "One Sample",
+        meta: { layout: "Alternative", title: "Sample Page" },
+        component: () => import(/* webpackChunkName: "one_sample_view" */ "../../components/views-partials/sample/OneSample.vue"),
+      },
+      {
+        path: "create",
+        name: "Create Sample",
+        meta: { layout: "Alternative", title: "Create Sample" },
+        component: () => import(/* webpackChunkName: "create_sample_view" */ "../../components/views-partials/sample/CreateSample.vue"),
+      },
+      {
+        path: ":id/edit",
+        name: "Edit Sample",
+        meta: { layout: "Alternative", title: "Edit Sample" },
+        component: () => import(/* webpackChunkName: "edit_sample_view" */ "../../components/views-partials/sample/EditSample.vue"),
+      },
     ],
   },
 ];
