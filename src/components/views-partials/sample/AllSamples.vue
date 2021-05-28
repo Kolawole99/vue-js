896c4ea763594dd2edb16e8defb483ba3bd05e32
Kolawole Ojo <philliphopeojo@gmail.com>
2021-05-28 03:14:49 +0100

    Sample requests setup

--- src/components/views-partials/sample/AllSamples.vue
+++ src/components/views-partials/sample/AllSamples.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <h1>Delete One Sample</h1>
+    {{ deleteOne }}
+    <h1>Delete Many Sample</h1>
+    {{ deleteMany }}
     <h1>All Samples</h1>
     {{ getAll }}
   </div>
@@ -12,12 +16,26 @@ export default {
   computed: {
     ...mapState({
       getAll: (state) => state.Sample.getAll,
+      deleteOne: (state) => state.Sample.deleteOne,
+      deleteMany: (state) => state.Sample.deleteMany,
     }),
   },
   mounted() {
     this.$store.dispatch("Sample/resetState");
-    let url = "client/";
+
+    let url;
+    url = "client/";
     this.$store.dispatch("Sample/getAll", url);
+
+    let data = {
+      id: ["12", "13", "14", "15"],
+      url: url,
+    };
+    this.$store.dispatch("Sample/deleteMany", data);
+
+    url = `client/${this.$route.params.id}`;
+    url = "client/12";
+    this.$store.dispatch("Sample/deleteOne", url);
   },
 };
 </script>
---
+++ src/components/views-partials/sample/EditSample.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Edit One Sample</h1>
+    {{ putOne }}
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "OneSample",
+  computed: {
+    ...mapState({
+      putOne: (state) => state.Sample.putOne,
+    }),
+  },
+  mounted() {
+    let data = {
+      url: `client/${this.$route.params.id}`,
+      type: "company",
+      website: "xyz.com",
+    };
+    this.$store.dispatch("Sample/putOne", data);
+  },
+};
+</script>
